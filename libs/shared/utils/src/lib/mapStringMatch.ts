fd1511fe538e64d55b9b5cec1f2de6be8afe5c71
NguyenVuNhan <nguyenvunhan1999@gmail.com>
2021-04-16 23:34:00 +0200

    Fix bugs when using query as a string

--- libs/shared/utils/src/lib/mapStringMatch.ts
+++ libs/shared/utils/src/lib/mapStringMatch.ts
@@ -3,11 +3,22 @@ export const mapStringMatch = <T>(
   query: string | RegExp,
   fn: (element: string, match: boolean, i?: number) => T
 ): (string | T)[] => {
-  const result = value.split(query);
+  let result = [];
+  if (query instanceof RegExp) {
+    result = value.split(query);
+  } else {
+    result = value
+      .split(query)
+      .reduce(
+        (result, value, index) =>
+          index !== 0 ? result.concat([query, value]) : result.concat(value),
+        []
+      );
+  }
 
   const ret: (string | T)[] = [...result];
 
-  for (let i = 1, length = result.length; i < length; i += 2) {
+  for (let i = 1; i < result.length; i += 2) {
     ret[i - 1] = fn(result[i - 1], false, i - 1);
     ret[i] = fn(result[i], true, i);
   }
