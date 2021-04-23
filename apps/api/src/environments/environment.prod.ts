38b7fe606a852ba48139114db4a30da369c9a966
NguyenVuNhan <nguyenvunhan1999@gmail.com>
2021-04-23 21:03:28 +0200

    Production environment for apps/api

--- apps/api/src/environments/environment.prod.ts
+++ apps/api/src/environments/environment.prod.ts
@@ -1,3 +1,19 @@
-export const environment = {
-  production: true,
+import { APIConfig } from '@cookingblog/api-interfaces';
+
+export const environment: APIConfig = {
+  port: parseInt(process.env.PORT),
+  isCORSEnabled: Boolean(process.env.CORS_ENABLED),
+  appSecret: process.env.APP_SECRET,
+  url: process.env.APP_URL,
+  maxUploadLimit: process.env.APP_MAX_UPLOAD_LIMIT,
+  maxParameterLimit: parseInt(process.env.APP_MAX_PARAMETER_LIMIT),
+  mongodbUrl: process.env.MONGOOSE_URL,
+  apiPrefix: process.env.API_PREFIX,
+  queueMonitor: true,
+  queueMonitorHttpPort: parseInt(process.env.QUEUE_HTTP_PORT),
+  redisPrefix: process.env.REDIS_QUEUE_PREFIX,
+  redisHttpPort: parseInt(process.env.REDIS_QUEUE_PORT),
+  redisHttpHost: process.env.REDIS_QUEUE_HOST,
+  redisDB: parseInt(process.env.REDIS_QUEUE_DB),
+  jwtExpiresIn: parseInt(process.env.JWT_EXPIRES_IN),
 };
