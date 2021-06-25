15782e64991ad736a24f9cb07ce4342284a1403a
NguyenVuNhan <nguyenvunhan1999@gmail.com>
2021-06-25 14:09:26 +0200

    Add environment.ts

---
+++ apps/api/src/environments/environment.ts
@@ -0,0 +1,34 @@
+import { APIConfig } from '../types';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: __dirname + '/../../.env.local' });
+
+export const environment: APIConfig = {
+  appName: 'Cooking Blog Api',
+  appVersion: '0.0.1',
+  production: false,
+  port: 5000,
+  isCORSEnabled: true,
+  appSecret: 'SeCrEt',
+  url: 'http://localhost:5000',
+  maxUploadLimit: '50mb',
+  maxParameterLimit: 5000,
+  mongodbUrl:
+    'mongodb://root:rootpassword@localhost:27017/cookingblog?authSource=admin',
+  apiPrefix: 'api',
+  queueMonitor: false,
+  queueMonitorHttpPort: 5550,
+  redisPrefix: 'queue',
+  redisHttpPort: 6379,
+  redisHttpHost: 'localhost',
+  redisDB: 3,
+  jwtExpiresIn: 60 * 24,
+  redisTimeout: 60,
+  spoonacularApiKeys: [
+    process.env.SPOONACULAR_API_KEY_1,
+    process.env.SPOONACULAR_API_KEY_2,
+    process.env.SPOONACULAR_API_KEY_3,
+    process.env.SPOONACULAR_API_KEY_4,
+    process.env.SPOONACULAR_API_KEY_5,
+  ],
+};
